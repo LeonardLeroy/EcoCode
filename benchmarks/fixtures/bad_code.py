74ba1bd0ffc049b090cb1c549d3b08d61b96edda
LeonardLeroy <leonard.leroy@epitech.eu>
2026-04-28 22:07:05 +0200

    test: add good code and bad code example scripts

---
+++ benchmarks/fixtures/bad_code.py
@@ -0,0 +1,10 @@
+import time
+
+def process_data():
+    data = []
+    for i in range(10000):
+        data.append(i)
+    return data
+
+if __name__ == "__main__":
+    process_data()
---
+++ benchmarks/fixtures/good_code.py
@@ -0,0 +1,5 @@
+def process_data():
+    return [i for i in range(10000)]
+
+if __name__ == "__main__":
+    process_data()
